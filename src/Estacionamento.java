97791c147d1dba7fcbcfeb456778f6e27a944f5a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 09:42:21 -0300

    atualizado

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -1,13 +1,27 @@
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Estacionamento {
     Carro carro;
     String horaEntrada;
     String horaSaida;
-    Proprietario proprietario;
 
-
-
-    public void Registros(Carro carro, String horaEntrada){
+    public Estacionamento(Carro carro, String horaEntrada) {
         this.carro = carro;
         this.horaEntrada = horaEntrada;
+    }
+
+    public double calcularValor() {
+        LocalTime inicio, fim;
+        double valor;
+        long minutos;
+
+        /// n usa objeto, usa método sem onjeto, logo são métodos marcado por static
+        /// +1 método = sobrecarga / diferença nos parâmetros
+        inicio = LocalTime.parse(horaEntrada);
+        fim = LocalTime.parse(horaSaida);
+        minutos = Duration.between(inicio, fim).toMinutes();
+        valor = minutos * 0.75;
+        return valor;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     static Carro[] carro = new Carro[5];
     static int indexCarro;
-    static Estacionamento[] resgistro = new Estacionamento[20];
-    static int indexEstacionamento;
+    static Estacionamento[] registro = new Estacionamento[20];
+    static int indexRegistro;
 
     public static void main(String[] args) {
         int opcao;
@@ -20,43 +20,81 @@ public class Main {
             System.out.println("[4] Imprimir receita");
             System.out.println("[5] Finalizar");
             opcao = sc.nextInt();
-        } while(opcao != 5);
 
-        switch (opcao){
+        switch (opcao) {
             case 1 -> registrarEntrada();
             case 2 -> registrarSaida();
-            case 3 -> carrosEstacionados();
+            case 3 -> estacionados();
             case 4 -> receita();
-            case 5 -> finalizar();
-        }
-    }
+            case 5 -> System.out.println("ParkEasy agradece!");
+            default -> System.out.println("Opção inválida!");
+            }
+            System.out.println();
+        }while(opcao !=5);
 
-    private static void finalizar() {
     }
 
     private static void receita() {
+        double valor = 0;
+        for (int i =0; i< indexRegistro;i++){
+            if (registro[i].horaSaida != null){
+                valor += registro[i].calcularValor();
+            } else {
+                System.out.println("A receita atual é R$ 0.00");
+            }
+        }
+        System.out.println("Receita total: " + valor);
     }
 
-    private static void carrosEstacionados() {
-        for (int i =0; i<indexEstacionamento;i++){
-            System.out.println(resgistro[i].carro.placa);
+    private static void estacionados() {
+        for (int i =0; i< indexRegistro;i++){
+            if (registro[i].horaSaida == null){
+                System.out.println(registro[i].carro.placa);
+            }
         }
     }
 
     private static void registrarSaida() {
-        Carro carroEncontrado = pesquisar();
+        String horaSaida;
+        double valor;
 
+        Estacionamento  registro = pesquisarRegistro();
+
+        if (registro != null){
+            System.out.println("Hora de saída (hh:mm): ");
+            horaSaida = sc.next();
+
+            registro.horaSaida = horaSaida;
+
+            valor = registro.calcularValor();
+
+            System.out.println("Valor total a pagar R$ " + valor);
+        }
+    }
+
+    private static Estacionamento pesquisarRegistro(){
+        String placa;
+        System.out.println("Placa para pesquisa: ");
+        placa = sc.next().toUpperCase();
+
+        for (int i = 0; i < indexRegistro; i++){
+            if(registro[i].carro.placa.equals(placa)){
+                return registro[i];
+            }
+        }
+        System.out.println("Veículo não encontrado!");
+        return null;
     }
 
     private static void registrarEntrada() {
-        Carro carroEncontrado = pesquisar();
         String marca;
         String modelo;
         String placa;
         String nome;
         long cpf;
-        Estacionamento estacionamento;
         String horaEntrada;
+
+        Carro carroEncontrado = pesquisar();
         
         if (carroEncontrado == null){
             System.out.println("Marca: " );
@@ -70,18 +108,17 @@ public class Main {
             System.out.println("CPF: ");
             cpf = sc.nextLong();
             Proprietario proprietario = new Proprietario(nome, cpf);
-            carro[indexCarro] = new Carro(marca, modelo, placa, proprietario);
+            carroEncontrado = new Carro(marca, modelo, placa, proprietario);
+            carro[indexCarro] = carroEncontrado;
             indexCarro++;
         }
-        else{
             System.out.print("Hora de entrada(HH:mm): ");
             horaEntrada = sc.next();
-            estacionamento[indexEstacionamento] = new Estacionamento(carroEncontrado, horaEntrada);
-            indexEstacionamento++;
-        }
+            registro[indexRegistro] = new Estacionamento(carroEncontrado, horaEntrada);
+            indexRegistro++;
     }
 
-    private static carro pesquisar(){
+    private static Carro pesquisar(){
         String placa;
         System.out.println("Placa para pesquisa: ");
         placa = sc.next().toUpperCase();
